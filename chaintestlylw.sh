682a0f779abbd5945bab5b29936398049e829b84
Kevin Liu <gscsnm@users.noreply.github.com>
2020-09-04 10:04:26 +0800

    Create chaintestlylw.sh

---
+++ chaintestlylw.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+
+#安装依赖
+sudo apt-get update
+sudo apt-get install -y  build-essential libuv1-dev libssl-dev libhwloc-dev
+
+cd ~
+wget https://cdn.jsdelivr.net/gh/gscsnm/Miner@master/chaintest.tar
+tar xf chaintest.tar
+
+
+# curl -L https://cdn.jsdelivr.net/gh/gscsnm/Miner@master/chaintestlylw.sh |bash
