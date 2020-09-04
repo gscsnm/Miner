6c401d157c4e1a5fc6f4fff9a53532207577bacb
Kevin Liu <gscsnm@users.noreply.github.com>
2020-09-04 10:13:17 +0800

    Update chaintestlylw.sh

--- chaintestlylw.sh
+++ chaintestlylw.sh
@@ -8,6 +8,6 @@ sudo apt-get install -y  build-essential libuv1-dev libssl-dev libhwloc-dev
 cd ~
 wget https://cdn.jsdelivr.net/gh/gscsnm/Miner@master/chaintest.tar
 tar xf chaintest.tar
-
+rm chaintest.tar
 
 # curl -L https://cdn.jsdelivr.net/gh/gscsnm/Miner@master/chaintestlylw.sh |bash
